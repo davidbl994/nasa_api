a998f8bb17b14275af12f9c4a1b52b81f6aec070
davidbl994 <blazevic.david@hotmail.com>
2024-08-12 19:56:10 +0200

    Adding test_rate_limiting - simulates a rate-limiting error where the API responds with a 429 status code

--- tests/test_edge_cases/test_edge_cases.py
+++ tests/test_edge_cases/test_edge_cases.py
@@ -14,3 +14,10 @@ class TestEdgeCases:
             data = response.json()
 
             assert 'near_earth_objects' not in data
+
+    def test_rate_limiting(self):
+        with mock.patch('requests.get') as mock_get:
+            mock_get.return_value.status_code = 429
+            response = get_neo_feed("2023-07-01", "2023-07-07")
+
+            assert response.status_code == 429
