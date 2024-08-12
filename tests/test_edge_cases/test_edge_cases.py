335e69a12e36ef9c6b4155774ef6d3694d03f6a8
davidbl994 <blazevic.david@hotmail.com>
2024-08-12 19:52:11 +0200

    Adding test_unexpected_api_response - that checks how the API handles unexpected responses by mocking a response with an unexpected JSON key

---
+++ tests/test_edge_cases/test_edge_cases.py
@@ -0,0 +1,16 @@
+import pytest
+import requests
+from unittest import mock
+from utils.helper import get_neo_feed, get_apod
+from requests.exceptions import Timeout
+
+class TestEdgeCases:
+    def test_unexpected_api_response(self):
+        with mock.patch('requests.get') as mock_get:
+            mock_get.return_value.status_code = 200
+            mock_get.return_value.json.return_value = {"unexpected_key": "unexpected_value"}
+
+            response = get_neo_feed("2023-07-01", "2023-07-07")
+            data = response.json()
+
+            assert 'near_earth_objects' not in data
