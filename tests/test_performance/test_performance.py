dde438e48d2394a73988e15722cd653ec8ff2849
davidbl994 <blazevic.david@hotmail.com>
2024-08-13 08:47:35 +0200

    Creating a test test_neo_feed_response_time to test the duration of API response

---
+++ tests/test_performance/test_performance.py
@@ -0,0 +1,13 @@
+import time
+from utils.helper import get_neo_feed, get_neo_lookup, get_apod
+
+class TestPerformance:
+
+    def test_neo_feed_response_time(self):
+        start_time = time.time()
+        response = get_neo_feed("2023-07-01", "2023-07-07")
+        end_time = time.time()
+        duration = end_time - start_time
+
+        assert response.status_code == 200
+        assert duration < 1.0, f"API response took too long: {duration}"
